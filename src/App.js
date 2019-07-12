0b32aeeb339709d78eed7d2641cba6edf4935069
Reborn <vubao1690@gmail.com>
2019-07-12 11:14:10 +0700

    Layout landing page

--- src/App.js
+++ src/App.js
@@ -5,29 +5,31 @@ import EmbedsElement from "./EmbedsElement";
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-          <div className="col-sm-6 center">
-            <EmbedsElement></EmbedsElement>
-          </div>
-      </div>
-      <div className="row">
-          <div className="col-sm-6">
-            <div>
-                <p>If you're interested, please leave your email here. We will inform you when we release</p>
-            </div>
-            <form>
-              <div className="form-group">
-                <label>Email</label>
-                <input type="email" className="form-control"></input>
-              </div> 
-              <div className="form-group">
-                <label>Feedback</label>
-                <textarea className="form-control"></textarea>
+    <div className="app-container">
+        <div className="row info-container">
+            <div className="col-sm info-child-container">
+              <div>
+                <h3>
+                    <span className="app-name">TripBFF</span> is coming soon.
+                </h3>
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>              
-          </div>
+              <div className="col-sm-7 headline-section">
+                <h2 style={{ "text-transform": "uppercase"}}>Your Trip experiences sharing community.</h2>
+                <h4>Time Saving. Auto Generate Timeline. Export Meaningful Infographic.</h4>
+              </div>
+              <div className="request-notify">
+                  <p>Wanted to be notified when we launch the amazing application?</p>
+              </div>
+              <div className="notify-outer-container">
+                  <input type="email" className="form-control email-field" placeholder="Enter your email"></input>
+                  <div className="btn-submit-container">
+                      <button type="submit" class="btn btn-primary">Notify me</button> 
+                  </div>                     
+              </div>          
+            </div>
+      </div>
+      <div className="col-sm-6 center video-embed-container">
+        <EmbedsElement></EmbedsElement>
       </div>
     </div>
   );
