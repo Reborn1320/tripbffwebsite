92d7c29960041971d107393b160ece4f5aae8874
Reborn <vubao1690@gmail.com>
2019-07-14 15:16:43 +0700

    Update video url

--- src/EmbedsElement.jsx
+++ src/EmbedsElement.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 const EmbedsElement = () => {
   return (
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/hpDQeHu1hW8" 
+      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/sOIuuI5qVBU" 
       allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true">
       </iframe>
     </div>
